0dd6aaf3b5032cf574c79b39dc7ad3cbd859ac62
MadsDamiri87 <madsc.damiri@gmail.com>
2026-03-15 13:10:56 +0100

    Nyt commit: Assignment 5.

--- Assignment/src/business/services/StockAlertService.java
+++ Assignment/src/business/services/StockAlertService.java
@@ -2,7 +2,6 @@ package business.services;
 
 import business.stockmarket.StockMarketListener;
 import business.stockmarket.simulation.LiveStock;
-import persistence.interfaces.UnitOfWork;
 import shared.logging.Logger;
 
 public class StockAlertService implements StockMarketListener
