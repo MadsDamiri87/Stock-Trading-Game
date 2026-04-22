acd59dd7a37658e827a8fefb7957e961f9457c35
MadsDamiri87 <madsc.damiri@gmail.com>
2026-04-22 06:57:44 +0200

    UI - Opgave 8 final

--- Assignment/src/presentation/state/UserSession.java
+++ Assignment/src/presentation/state/UserSession.java
@@ -4,18 +4,8 @@ import java.util.UUID;
 
 public class UserSession
 {
-  private String traderName;
   private UUID activePortfolioId;
 
-  public String getTraderName()
-  {
-    return traderName;
-  }
-
-  public void setTraderName(String traderName)
-  {
-    this.traderName = traderName;
-  }
 
   public UUID getActivePortfolioId()
   {
