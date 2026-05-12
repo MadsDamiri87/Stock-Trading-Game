c6f04c9bdf3f1d04cc0c793f72bb641967d0d0be
MadsDamiri87 <madsc.damiri@gmail.com>
2026-05-12 12:34:22 +0200

    update testing results

---
+++ Assignment/test/src/Main.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+  public static void main(String[] args)
+  {
+
+
+  }
+}
